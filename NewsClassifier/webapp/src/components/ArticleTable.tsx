4bd75ae8bd7cecf9973644d51b4be5376d442699
martarzy <marta.bandarzy@gmail.com>
2017-05-13 01:40:11 +0200

    Pagination

--- NewsClassifier/webapp/src/components/ArticleTable.tsx
+++ NewsClassifier/webapp/src/components/ArticleTable.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Table } from "react-bootstrap";
+import { Table, Pagination } from "react-bootstrap";
 
 export type ArticleRating = "agree" | "disagree" | "discuss" | "unrelated";
 
@@ -17,7 +17,16 @@ export interface ArticleTableProps {
     onShowArticleDetails?: (index: number) => void;
 }
 
-export class ArticleTable extends React.Component<ArticleTableProps, undefined> {
+export interface ArticleTableState {
+    currentPage: number;
+}
+
+export class ArticleTable extends React.Component<ArticleTableProps, ArticleTableState> {
+    constructor() {
+        super();
+        this.state = { currentPage: 0 };
+    }
+
     private prettyTime(epoch: number): string
     {
         return new Date(epoch).toLocaleString();
@@ -44,9 +53,16 @@ export class ArticleTable extends React.Component<ArticleTableProps, undefined> 
         else
             return "bg-danger"
     }
+
+    private handleSelect(pageNo: number) {
+        this.setState({
+            currentPage: pageNo
+        });
+    }
     
     render() {
-        let rows = this.props.articles.map((value: ArticleItem, index: number, array: ArticleItem[]) => {
+        let rows = this.props.articles.slice(this.state.currentPage*10, (this.state.currentPage+1)*10-1)
+                    .map((value: ArticleItem, index: number, array: ArticleItem[]) => {
             return (
                  <tr className={this.bgRating(value.rating)}>
                     <td><a href="#" onClick={() => this.props.onShowArticleDetails(index)}>{value.heading}</a></td>
@@ -56,16 +72,31 @@ export class ArticleTable extends React.Component<ArticleTableProps, undefined> 
         });
 
         return (
-            <Table striped bordered condensed hover responsive>
-                <thead>
-                    <th width="70%">Title</th>
-                    <th width="15%">Date</th>
-                    <th width="15%">Rate</th>
-                </thead>
-                <tbody>
-                    {rows}
-                </tbody>
-            </Table>
+            <div>
+                <div className="text-center">
+                    <Pagination
+                        prev
+                        next
+                        first
+                        last
+                        ellipsis
+                        boundaryLinks
+                        items={Math.floor(this.props.articles.length/10)}
+                        maxButtons={5}
+                        activePage={this.state.currentPage}
+                        onSelect={this.handleSelect.bind(this)} />
+                </div>
+                <Table striped bordered condensed hover responsive>
+                    <thead>
+                        <th width="70%">Title</th>
+                        <th width="15%">Date</th>
+                        <th width="15%">Rate</th>
+                    </thead>
+                    <tbody>
+                        {rows}
+                    </tbody>
+                </Table>
+            </div>
         );
     }
 }
